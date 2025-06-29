ff8914d6df6190d2513890c9dc138bb2eedce737
blairjdaniel <97918300+blairjdaniel@users.noreply.github.com>
2025-06-29 15:45:19 -0700

    Delete src directory

--- src/components/ConnectButton.tsx
+++
@@ -1,6 +0,0 @@
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import "@solana/wallet-adapter-react-ui/styles.css";
-
-export function ConnectButton() {
-  return <WalletMultiButton />;
-}
--- src/helpers/logs.tsx
+++
@@ -1,6 +0,0 @@
-export function logMintContext(wallet: any, candyMachineV3: any, umi?: any) {
-  console.log("umi:", umi);
-  console.log("wallet:", wallet);
-  console.log("candyMachineV3:", candyMachineV3);
-  console.log("candyMachineV3.candyMachine:", candyMachineV3?.candyMachine);
-}
