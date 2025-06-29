32d29e3715262330cf8e51bc8e14205ac1557dcb
blairjdaniel <97918300+blairjdaniel@users.noreply.github.com>
2025-06-29 15:45:04 -0700

    Delete pages directory

--- pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import "@solana/wallet-adapter-react-ui/styles.css";
-import "../styles/globals.css";
-import '../styles/modal.css';
-
-
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
-export default App;
